a8b059ec158a6db65af92551b7a237baf8e7f44d
Gabriel Franco da Rocha Nascimento <gabrielfrna@yahoo.com.br>
2022-12-17 17:46:35 -0300

    Criação dos arquivos para utilização do Ansible

--- main.tf
+++ main.tf
@@ -17,7 +17,13 @@ resource "aws_instance" "app_server" {
   ami           = "ami-0a6b2839d44d781b2"
   instance_type = "t2.micro"
   key_name = "iac-alura"
+  # user_data = <<-EOF
+  #                #!/bin/bash
+  #                cd /home/ubuntu
+  #                echo "<h1>Feito com Terraform</h1>" > index.html
+  #                nohup busybox httpd -f -p 8080 &
+  #                EOF
   tags = {
-    Name = "Primeira instancia"
+    Name = "Teste aws"
   }
 }
